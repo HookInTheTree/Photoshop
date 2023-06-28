829091ffb6c53ad40de8c10d9c7d54d98e042b8f
Chera Dz <dzheriev2003@mail.ru>
2023-06-28 12:02:12 +0300

    free rotation filter

---
+++ Parameters/RotationParameters.cs
@@ -0,0 +1,32 @@
+﻿using MyPhotoshop.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhotoshop.Parameters
+{
+    public class RotationParameters : IParameters
+    {
+        public double Angle { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new ParameterInfo[] {
+                new ParameterInfo {
+                    Name = "Градус поворота",
+                    DefaultValue = 0,
+                    MaxValue = 360,
+                    MinValue = 0,
+                    Increment = 1
+                }
+             };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Angle = values[0];
+        }
+    }
+}
